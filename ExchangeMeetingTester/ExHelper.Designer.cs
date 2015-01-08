b519b5c94349e7427d0e9b6b4f9d6b04d0538037
shuanshuan <zhangzhangang@hotmail.com>
2015-01-08 17:15:12 +0800

    new create

---
+++ ExchangeMeetingTester/ExHelper.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace ExchangeMeetingTester
+{
+    partial class ExHelper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btAction = new System.Windows.Forms.Button();
+            this.rtbMessage = new System.Windows.Forms.RichTextBox();
+            this.ddFolder = new System.Windows.Forms.ComboBox();
+            this.btConnect = new System.Windows.Forms.Button();
+            this.tbPwd = new System.Windows.Forms.TextBox();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btAction
+            // 
+            this.btAction.Location = new System.Drawing.Point(237, 58);
+            this.btAction.Name = "btAction";
+            this.btAction.Size = new System.Drawing.Size(82, 21);
+            this.btAction.TabIndex = 4;
+            this.btAction.Text = "Delete";
+            this.btAction.UseVisualStyleBackColor = true;
+            this.btAction.Click += new System.EventHandler(this.btAction_Click);
+            // 
+            // rtbMessage
+            // 
+            this.rtbMessage.Location = new System.Drawing.Point(0, 89);
+            this.rtbMessage.Name = "rtbMessage";
+            this.rtbMessage.Size = new System.Drawing.Size(442, 429);
+            this.rtbMessage.TabIndex = 5;
+            this.rtbMessage.Text = "";
+            // 
+            // ddFolder
+            // 
+            this.ddFolder.FormattingEnabled = true;
+            this.ddFolder.Location = new System.Drawing.Point(99, 58);
+            this.ddFolder.Name = "ddFolder";
+            this.ddFolder.Size = new System.Drawing.Size(121, 21);
+            this.ddFolder.TabIndex = 3;
+            // 
+            // btConnect
+            // 
+            this.btConnect.Location = new System.Drawing.Point(354, 25);
+            this.btConnect.Name = "btConnect";
+            this.btConnect.Size = new System.Drawing.Size(75, 23);
+            this.btConnect.TabIndex = 2;
+            this.btConnect.Text = "Connect";
+            this.btConnect.UseVisualStyleBackColor = true;
+            this.btConnect.Click += new System.EventHandler(this.btConnect_Click);
+            // 
+            // tbPwd
+            // 
+            this.tbPwd.Location = new System.Drawing.Point(237, 26);
+            this.tbPwd.Name = "tbPwd";
+            this.tbPwd.PasswordChar = '*';
+            this.tbPwd.Size = new System.Drawing.Size(100, 20);
+            this.tbPwd.TabIndex = 1;
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Location = new System.Drawing.Point(56, 26);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(119, 20);
+            this.tbEmail.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(181, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Password: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Email: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "EmailFolder: ";
+            // 
+            // ExHelper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 517);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.tbPwd);
+            this.Controls.Add(this.btConnect);
+            this.Controls.Add(this.ddFolder);
+            this.Controls.Add(this.rtbMessage);
+            this.Controls.Add(this.btAction);
+            this.Name = "ExHelper";
+            this.Text = "ExHelper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btAction;
+        private System.Windows.Forms.RichTextBox rtbMessage;
+        private System.Windows.Forms.ComboBox ddFolder;
+        private System.Windows.Forms.Button btConnect;
+        private System.Windows.Forms.TextBox tbPwd;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ExchangeMeetingTester/ExHelper.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Exchange.WebServices.Data;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Net;
+using System.Configuration;
+
+
+
+
+namespace ExchangeMeetingTester
+{
+    public partial class ExHelper : Form
+    {
+        ExchangeService service;
+        public ExHelper()
+        {
+            InitializeComponent();
+            try
+            {
+                this.tbEmail.Text = ConfigurationManager.AppSettings["Email"];
+                this.tbPwd.Text = ConfigurationManager.AppSettings["Password"];
+                string folders = ConfigurationManager.AppSettings["Folders"];
+                this.ddFolder.DataSource = folders.Split(',');
+                this.ddFolder.SelectedIndex = 0;
+            }
+            catch (FieldAccessException)
+            {
+                this.rtbMessage.AppendText("Access App.config is denied! \n");
+            }
+            catch (FormatException)
+            {
+                this.rtbMessage.AppendText("App.config is not well formated! \n");
+
+            }
+
+            this.btAction.Enabled = false;
+        }
+
+        private void btAction_Click(object sender, EventArgs e)
+        {
+            //FolderId fId = new FolderId(WellKnownFolderName.Inbox, new Mailbox(email));
+            string value = this.ddFolder.SelectedItem.ToString();
+            WellKnownFolderName fname = (WellKnownFolderName)Enum.Parse(typeof(WellKnownFolderName), value);
+            Folder folder = Folder.Bind(service, fname);
+            folder.Empty(DeleteMode.HardDelete, true);
+            this.rtbMessage.AppendText("All the items in this folder has been deleted! \n");
+        }
+
+        private void ReadMeetings(DateTime startDate, DateTime endDate, FolderId fId)
+        {
+            //FolderId fId = new FolderId(WellKnownFolderName.Calendar, new Mailbox(email));
+            CalendarFolder calendar = CalendarFolder.Bind(service, fId, new PropertySet());
+            CalendarView cView = new CalendarView(startDate, endDate);
+            cView.PropertySet = new PropertySet(AppointmentSchema.Subject, AppointmentSchema.Start, AppointmentSchema.End);
+
+            FindItemsResults<Appointment> appointments = calendar.FindAppointments(cView);
+
+            foreach (Appointment a in appointments)
+            {
+                rtbMessage.AppendText("Subject: " + a.Subject.ToString() + " \n");
+                rtbMessage.AppendText("Start: " + a.Start.ToString() + " \n");
+                rtbMessage.AppendText("End: " + a.End.ToString() + "\n");
+            }
+        }
+
+        private ExchangeService ServiceIni(string email, string password)
+        {
+            ServicePointManager.ServerCertificateValidationCallback = CertificateValidationCallBack;
+            ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2010_SP2);
+            service.Credentials = new WebCredentials(email, password);
+            service.UseDefaultCredentials = false;
+            service.TraceEnabled = true;
+            service.TraceFlags = TraceFlags.All;
+            service.Timeout = int.Parse(ConfigurationManager.AppSettings["TimeOut"]);
+            service.AutodiscoverUrl(email, RedirectionUrlValidationCallback);
+            return service;
+        }
+
+
+        private static bool RedirectionUrlValidationCallback(string redirectionUrl)
+        {
+            // The default for the validation callback is to reject the URL.
+            bool result = false;
+
+            Uri redirectionUri = new Uri(redirectionUrl);
+
+            // Validate the contents of the redirection URL. In this simple validation
+            // callback, the redirection URL is considered valid if it is using HTTPS
+            // to encrypt the authentication credentials. 
+            if (redirectionUri.Scheme == "https")
+            {
+                result = true;
+            }
+            return result;
+        }
+
+        private static bool CertificateValidationCallBack(
+                object sender,
+                System.Security.Cryptography.X509Certificates.X509Certificate certificate,
+                System.Security.Cryptography.X509Certificates.X509Chain chain,
+                System.Net.Security.SslPolicyErrors sslPolicyErrors)
+        {
+            // If the certificate is a valid, signed certificate, return true.
+            if (sslPolicyErrors == System.Net.Security.SslPolicyErrors.None)
+            {
+                return true;
+            }
+
+            // If there are errors in the certificate chain, look at each error to determine the cause.
+            if ((sslPolicyErrors & System.Net.Security.SslPolicyErrors.RemoteCertificateChainErrors) != 0)
+            {
+                if (chain != null && chain.ChainStatus != null)
+                {
+                    foreach (System.Security.Cryptography.X509Certificates.X509ChainStatus status in chain.ChainStatus)
+                    {
+                        if ((certificate.Subject == certificate.Issuer) &&
+                           (status.Status == System.Security.Cryptography.X509Certificates.X509ChainStatusFlags.UntrustedRoot))
+                        {
+                            // Self-signed certificates with an untrusted root are valid. 
+                            continue;
+                        }
+                        else
+                        {
+                            if (status.Status != System.Security.Cryptography.X509Certificates.X509ChainStatusFlags.NoError)
+                            {
+                                // If there are any other errors in the certificate chain, the certificate is invalid,
+                                // so the method returns false.
+                                return false;
+                            }
+                        }
+                    }
+                }
+
+                // When processing reaches this line, the only errors in the certificate chain are 
+                // untrusted root errors for self-signed certificates. These certificates are valid
+                // for default Exchange server installations, so return true.
+                return true;
+            }
+            else
+            {
+                // In all other cases, return false.
+                return false;
+            }
+        }
+
+        private void roomList_Click(object sender, EventArgs e)
+        {
+
+            // Return all the room lists in the organization.
+            EmailAddressCollection myRoomLists = service.GetRoomLists();
+
+            // Display the room lists.
+            foreach (EmailAddress address in myRoomLists)
+            {
+                string t = string.Format("Email Address: {0} Mailbox Type: {1} \n", address.Address, address.MailboxType);
+                this.rtbMessage.AppendText(t);
+            }
+
+
+
+
+        }
+
+        private void btConnect_Click(object sender, EventArgs e)
+        {
+            string dis = "disConnect";
+            if (this.btConnect.Text != dis)
+            {
+                this.btConnect.Text = dis;
+                Connecting();
+            }
+            else {
+                this.btConnect.Text = "Connect";
+                this.btAction.Enabled = false;
+                service = null;
+            }
+            
+        }
+
+        private void Connecting()
+        {
+
+            this.rtbMessage.AppendText("Connecting \n");
+            string email = this.tbEmail.Text;
+            string pwd = this.tbPwd.Text;
+            try
+            {
+                service = ServiceIni(email, pwd);
+                this.rtbMessage.AppendText("Connected! \n");
+                this.btAction.Enabled = true;
+            }
+          //  catch (FormatException)
+         //   {
+           //     MessageBox.Show("It's not a well format email address!");
+         //   }
+            catch (TimeoutException)
+            {
+                MessageBox.Show("Connection Time out!");
+            }
+            catch (AutodiscoverLocalException)
+            {
+                MessageBox.Show("Bad username or password!");
+            }
+        }
+
+  
+
+    }
+}
